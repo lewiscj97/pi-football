cbe2f85ef4ab4a065592cce25d18842f0fbaa507
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-06 21:45:16 +0100

    feat(*): write main class for scraping and formatting

--- football/main.py
+++ football/main.py
@@ -8,4 +8,5 @@ if __name__ == '__main__':
     matches = scraper.matches
 
     displayer = DisplayText()
-    displayer.display(matches[0].to_array)
+    print(matches[0])
+    # displayer.display(matches[0].to_array)
